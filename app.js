51be7ff533a5bfd2c3898d419a707ac6072b082f
Rohan Gada <gadarohan17@gmail.com>
2017-02-18 13:46:35 +0530

    now has config in local

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var flock = require('flockos')
+var flock = require('flockos');
+var flock = require('./config.js');
 flock.appId = '<app id>';
 flock.appSecret = '<app secret>';
 flock.events.verifyToken(token);
